4b8a5c2b60006a0760897674231a8af2d1f951fe
KVellik <wellik@tlu.ee>
2023-09-20 15:56:50 +0300

    Algne setup, Hello.js ja Maths.js

---
+++ src/components/Hello.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+//const Hello = props => {
+//  return <div>Tere {props.name}</div>
+//}
+
+/* const Hello = props => {
+  const { name } = props
+  return <div>Tere {name}</div>
+} */
+
+const Hello = ({ name }) => {
+  ;<div>Hello {name}!</div>
+}
+export default Hello
