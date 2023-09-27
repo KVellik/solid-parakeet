2531d6c7571907b849c9fb78a74ab02e3d59b497
KVellik <wellik@tlu.ee>
2023-09-27 13:59:11 +0300

    lisatud toggle nupp, form

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import "./App.css"
-import Hello from "./components/Hello"
+import { useState } from "react"
+import Toggle from "./components/Toggle"
+import Show from "./components/Show"
 import Maths from "./components/Maths"
+import MyForm from "./components/MyForm"
 
 function App() {
+  const [show, setShow] = useState(true)
+
+  const toggleShow = () => setShow(previousShow => !previousShow)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <Maths></Maths>
-        <Maths></Maths>
-      </header>
+    <div className="Container">
+      <h1>Page</h1>
+      <MyForm />
+      <Maths />
+      <Show show={show} />
+      <Toggle toggleShow={toggleShow} />
     </div>
   )
 }
